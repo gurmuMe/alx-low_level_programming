766ce75ab3a23227e5a5a1d5fb0fdba81b9003d3
Gurmu Gessese <gurmume@gmail.com>
2023-06-21 11:22:32 -0700

    1-alphabet commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,21 @@
-/**
- * print_alphabet - Prints all lowercase alphabets.
- *
- * Description: This function prints all the lowercase alphabets from 'a' to 'z'.
- */
-void print_alphabet(void)
-{
-    char letter;
+#include "main.h"
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-        _putchar(letter);
 
-    _putchar('\n');
-}
+	/**
+	 * print_alphabet - print all alphabet in lowercase
+	 *
+	 * * Description: This function prints all the lowercase alphabets from 'a' to 'z'.
+	 */
+
+
+	void print_alphabet(void)
+	{
+		char letter;
+
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+
+		_putchar('\n');
+	}
