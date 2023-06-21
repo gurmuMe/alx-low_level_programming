270426707e719c558495e1d6f8a4ab0159adc424
Gurmu Gessese <gurmume@gmail.com>
2023-06-21 11:40:08 -0700

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 
-	/**
-	 * print_alphabet - print all alphabet in lowercase
-	 *
-	 * * Description: This function prints all
-	 *  the lowercase alphabets from 'a' to 'z'.
-	 */
+/**
+* print_alphabet - print all alphabet in lowercase
+*
+* * Description: This function prints all
+*  the lowercase alphabets from 'a' to 'z'.
+*/
 
 
 	void print_alphabet(void)
